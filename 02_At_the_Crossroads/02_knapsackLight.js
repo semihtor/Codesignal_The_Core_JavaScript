f25ec8eaf79a43e3b9a2c9246de68dce2fac2133
Semih Tor <82044766+semihtor@users.noreply.github.com>
2021-07-01 15:57:40 +0300

    Create 02_knapsackLight.js

---
+++ 02_At_the_Crossroads/02_knapsackLight.js
@@ -0,0 +1,14 @@
+function knapsackLight(value1, weight1, value2, weight2, maxW) {
+    
+    if(weight1 + weight2 <= maxW){
+        return value1 + value2;
+    }else if(weight1 <= maxW && weight2 > maxW){
+		return value1;
+	}else if(weight2 <= maxW && weight1 > maxW){
+		return value2;
+	}else if(weight1 > maxW && weight2 > maxW){
+		return 0;
+	}else{
+		return value2 > value1 ? value2 : value1;
+	}
+}
