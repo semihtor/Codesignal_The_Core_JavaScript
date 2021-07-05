d0b797ae1b25705fb9046f93e1b7e08bd57a0003
Semih Tor <82044766+semihtor@users.noreply.github.com>
2021-07-05 09:24:29 +0300

    Create 05_additionWithoutCarrying.js

---
+++ 04_Loop_Tunnel/05_additionWithoutCarrying.js
@@ -0,0 +1,15 @@
+function additionWithoutCarrying(param1, param2) {
+
+    var result = '';
+
+    while(param1 || param2){
+    
+        result = (((param1%10)+(param2%10))%10).toString() + result;
+        
+        param1 = Math.floor(param1/10);
+        param2 = Math.floor(param2/10);
+    }
+    
+  return Number(result);
+  
+}
