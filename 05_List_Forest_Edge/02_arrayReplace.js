f9e720948068231783ade015f30d6fda6e53bf8a
Semih Tor <82044766+semihtor@users.noreply.github.com>
2021-07-05 09:30:29 +0300

    Create 02_arrayReplace.js

---
+++ 05_List_Forest_Edge/02_arrayReplace.js
@@ -0,0 +1,12 @@
+function arrayReplace(inputArray, elemToReplace, substitutionElem) {
+
+    for(var i = 0; i < inputArray.length; i++){
+        
+        if(inputArray[i] == elemToReplace){
+
+            inputArray[i] = substitutionElem;
+        }
+    }
+
+    return inputArray;
+}
